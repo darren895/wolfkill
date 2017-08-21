d4d1277a487f51293ea26072b4b54a655446faa6
zhuhuajun <darren895@gmail.com>
2017-08-21 17:14:47 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,20 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    content: '狼人杀助手'
   },
   //事件处理函数
-  bindViewTap: function() {
+  createRoom: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../createRoom/createRoom'
+    })
+  },
+  joinRoom:function() {
+    wx.navigateTo({
+      url: '../joinRoom/joinRoom',
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    
   }
 })
